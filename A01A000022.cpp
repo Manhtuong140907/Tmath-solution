37f8b39dc5cdf76a9e4d4505ef106a8fe6338a5d
MTuong <tuongden1409@gmail.com>
2025-07-05 20:56:44 +0700

    Create A01A000022.cpp

---
+++ A01A000022.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+#define fi first
+#define se second 
+#define _nhmt_ main()
+#define docnhanh ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define mod 1000000007
+
+using namespace std;
+
+//===============declare===============
+
+long long n,m,res;
+long long u,v,dem;
+long long a[1000005];
+long long t,x,y;
+string s;
+
+//===============function==============
+void mo()
+{
+    freopen("nhap.inp","r",stdin);
+    freopen("nhap.out","w",stdout);
+}
+//================code=================
+
+_nhmt_
+{
+    //mo();
+    docnhanh
+    cin>>x>>y>>t;
+    cout<<x*y*t;
+    return 0;
+}
+/*===============end===================
+Code by vinh_nguyenhuumanhtuong
+*/
